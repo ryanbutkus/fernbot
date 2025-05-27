3bf1c1783f6c51229d5682e2acc5bf695887af15
Ryan Butkus <ryanbutkus@gmail.com>
2025-05-27 02:06:53 +0000

    initial version

---
+++ twitchcheck.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import fern_keys
+import requests
+
+client_id = fern_keys.twitch_client_id
+client_secret = fern_keys.twitch_client_secret
+streamer = 'princess_jem4'
+
+# OAuth token URL and data
+url = 'https://id.twitch.tv/oauth2/token'
+payload = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'grant_type': 'client_credentials'
+}
+
+get_token_headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+access_token_response = requests.post(url, data=payload, headers=get_token_headers)
+# Parse the response
+if access_token_response.status_code == 200:
+    access_token = access_token_response.json().get('access_token')
+
+get_status_headers = {
+    'Client-ID': client_id,
+    'Authorization': f'Bearer {access_token}'
+}
+
+url = f'https://api.twitch.tv/helix/streams?user_login={streamer}'
+response = requests.get(url, headers=get_status_headers)
+data = response.json()
+
+if data['data']:
+    print(f"{streamer} is live!")
+else:
+    print(f"{streamer} is offline.")
